711b68bea98a6070456370ea4b8677ae71447ae0
mythrisrinivas <121863207+mythrisrinivas@users.noreply.github.com>
2023-04-21 12:55:03 +0530

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "the-server-samurai-s3"
-    key    = "demo/terraform.tfstate"
+    key    = "demo1/terraform.tfstate"
     region = "ap-southeast-1"
   }
 }
