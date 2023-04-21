8df535d527bf9f825ce3446b50abe1deb085f6ce
Keerthana <keerthana.r@thoughtworks.com>
2023-04-21 12:15:05 +0530

    [Infra-3] Removing kubernetes deployment

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -2,20 +2,12 @@ provider "aws" {
   region = "ap-southeast-1"
 }
 
-data "aws_eks_cluster_auth" "eks-cluster-auth" {
-  name =  aws_eks_cluster.tss-cluster.name
-}
+#data "aws_eks_cluster_auth" "eks-cluster-auth" {
+#  name =  aws_eks_cluster.tss-cluster.name
+#}
 
-provider "kubernetes" {
-  host                   = aws_eks_cluster.tss-cluster.endpoint
-  cluster_ca_certificate = base64decode(aws_eks_cluster.tss-cluster.certificate_authority.0.data)
-  token                  = data.aws_eks_cluster_auth.eks-cluster-auth.token
-}
-
-output "aws_eks_cluster" {
-  value = aws_eks_cluster.tss-cluster
-}
-
-output "auth" {
-  value = data.aws_eks_cluster_auth.eks-cluster-auth
-}
+#provider "kubernetes" {
+#  host                   = aws_eks_cluster.tss-cluster.endpoint
+#  cluster_ca_certificate = base64decode(aws_eks_cluster.tss-cluster.certificate_authority.0.data)
+#  token                  = data.aws_eks_cluster_auth.eks-cluster-auth.token
+#}
